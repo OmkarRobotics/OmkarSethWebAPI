62f11b7f0e58ba7a7bd7618328e8e7fbc86047e4
OmkarRobotics <122411633+OmkarRobotics@users.noreply.github.com>
2024-07-14 12:26:09 -0400

    Added Name Controller

---
+++ OmkarSethWebAPI/Controllers/NameController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OmkarSethWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NameController : ControllerBase
+    {
+        [HttpGet(Name = "GetName")]
+        public IActionResult Get()
+        {
+            var name = new { Name = "Omkar Seth" };
+            return Ok(name);
+        }
+    }
+}
